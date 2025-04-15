e7b1a878dbeeedf1a870c0912d17809334465d17
thuchanh1 <ductuan02072005@gmail.com>
2025-04-15 20:58:29 +0700

    git

--- src/main/java/Order/Modal/OrderMain.java
+++ src/main/java/Order/Modal/OrderMain.java
@@ -1,17 +1,36 @@
 package Order.Modal;
 
+import com.formdev.flatlaf.FlatClientProperties;
+import com.formdev.flatlaf.FlatLaf;
+import com.formdev.flatlaf.fonts.roboto.FlatRobotoFont;
+import raven.modal.Drawer;
+
+import javax.swing.*;
+import java.awt.*;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class OrderMain {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+public class OrderMain extends javax.swing.JFrame {
+    public static final String DEMO_VERSION = "2.4.1-SNAPSHOT";
+    public OrderMain() {
+        init();
     }
+    private void init() {
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        getRootPane().putClientProperty(FlatClientProperties.FULL_WINDOW_CONTENT, true);
+        Drawer.installDrawer(this, new MyDrawerBuilder());
+        FormManager.install(this);
+        setSize(new Dimension(1366, 768));
+        setLocationRelativeTo(null);
+    }
+
+    public static void main(String[] args) {
+        DemoPreferences.init();
+        FlatRobotoFont.install();
+        FlatLaf.registerCustomDefaultsSource("raven.modal.demo.themes");
+        UIManager.put("defaultFont", new Font(FlatRobotoFont.FAMILY, Font.PLAIN, 13));
+        DemoPreferences.setupLaf();
+        EventQueue.invokeLater(() -> new Demo().setVisible(true));
+    }
+
 }
