a094bff005cb3bc8af30e37e6338ed30cc0f549b
Khoa <caovankhoa11@gmail.com>
2025-04-15 20:57:50 +0700

    add LoginForm

--- src/main/java/Order/Modal/Auth/LoginForm.java
+++ src/main/java/Order/Modal/Auth/LoginForm.java
@@ -16,7 +16,7 @@ public class LoginForm extends JPanel {
     {
         setLayout(new MigLayout("wrap,gapy 3","[fill,300]"));
 
-        add(new JLabel(new FlatSVGIcon("Order/Login/icon/logo.svg",1.5f)));
+        add(new JLabel(new FlatSVGIcon("Order/icons/logo.svg",1.5f)));
         JLabel lbTitle= new JLabel("Welcome back", JLabel.CENTER);
         lbTitle.putClientProperty(FlatClientProperties.STYLE,""+"font:bold +15;");
         add(lbTitle, "gapy 8 8");
@@ -41,7 +41,7 @@ public class LoginForm extends JPanel {
         txtEmail.putClientProperty(FlatClientProperties.STYLE, ""
                 +"iconTextGap:10;");
         txtEmail.putClientProperty(FlatClientProperties.PLACEHOLDER_TEXT,"Enter your email");
-        txtEmail.putClientProperty(FlatClientProperties.TEXT_FIELD_LEADING_ICON, new FlatSVGIcon("Order/Login/icon/email.svg",0.35f));
+        txtEmail.putClientProperty(FlatClientProperties.TEXT_FIELD_LEADING_ICON, new FlatSVGIcon("Order/icons/email.svg",0.35f));
         add(txtEmail);
 
         JLabel lbPassword= new JLabel("Password");
@@ -54,7 +54,7 @@ public class LoginForm extends JPanel {
                 +"iconTextGap:10;"+
                 "showRevealButton:true;");
         txtPassword.putClientProperty(FlatClientProperties.PLACEHOLDER_TEXT,"Enter your password");
-        txtPassword.putClientProperty(FlatClientProperties.TEXT_FIELD_LEADING_ICON, new FlatSVGIcon("Order/Login/icon/password.svg",0.35f));
+        txtPassword.putClientProperty(FlatClientProperties.TEXT_FIELD_LEADING_ICON, new FlatSVGIcon("Order/icons/password.svg",0.35f));
         //txtPassword.putClientProperty(FlatClientProperties.TEXT_FIELD_TRAILING_ICON, new FlatSVGIcon("Order/Login/icon/eye.svg",0.6f));
         JLabel eyeIcon = new JLabel(new FlatSVGIcon("Order/Login/icon/eye.svg", 0.6f));
         txtPassword.putClientProperty(FlatClientProperties.TEXT_FIELD_TRAILING_ICON, eyeIcon);
@@ -76,7 +76,7 @@ public class LoginForm extends JPanel {
 
         add(new JCheckBox("Remember me"), "gapy 10 10");
 
-        JButton cmdSignIn= new JButton("Sign in", new FlatSVGIcon("Order/Login/icon/next.svg")){
+        JButton cmdSignIn= new JButton("Sign in", new FlatSVGIcon("Order/icons/next.svg")){
             @Override
             public boolean isDefaultButton(){
                 return true;
