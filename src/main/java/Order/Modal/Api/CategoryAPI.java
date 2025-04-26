49dcaf25f871958f2b87f2ee27b082c49e2caaf6
Hi <hungthanhhung37@gmail.com>
2025-04-26 18:00:51 +0700

    feat(category) : add search to form

--- src/main/java/Order/Modal/Api/CategoryAPI.java
+++ src/main/java/Order/Modal/Api/CategoryAPI.java
@@ -1,6 +1,7 @@
 package Order.Modal.Api;
 
 import Order.Modal.Entity.categories;
+import Order.Modal.Response.ApiResponse;
 import Order.Modal.Response.CategoryResponse;
 import Order.Modal.Response.CreateCategoryResponse;
 import Order.Modal.Response.DeleteCategoryResponse;
@@ -14,11 +15,12 @@ public interface CategoryAPI {
     Call<CategoryResponse> getAllCategories();
     @GET("/api/category/{id}")
     Call<CreateCategoryResponse> getCategoryId(@Path("id") String id);
+    @GET("api/category/search")
+    Call<ApiResponse<List<categories>>> getCategoriesBySearch(@Query("keyword") String keyword);
     @POST("/api/category")
     Call<CreateCategoryResponse> addCategory(@Body categories category);
     @DELETE("/api/category/{id}")
     Call<DeleteCategoryResponse> deleteCategory(@Path("id") String id);
     @PATCH("/api/category/{id}")
     Call<CreateCategoryResponse> updateCategory(@Path("id") String id, @Body categories category);
-
 }
