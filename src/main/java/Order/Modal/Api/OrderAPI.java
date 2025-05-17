930ba11a024f8263f187d9080954c28ff2ab3a66
hungheo <duongthanhhung1@dtu.edu.vn>
2025-05-17 15:36:19 +0700

    feat(order_items): update -  items , delete items , show ui edit and load item

--- src/main/java/Order/Modal/Api/OrderAPI.java
+++ src/main/java/Order/Modal/Api/OrderAPI.java
@@ -1,11 +1,14 @@
 package Order.Modal.Api;
 
 import Order.Modal.Entity.orders;
+import Order.Modal.Response.ApiResponse;
 import Order.Modal.Response.orders.CreatedOrderResponse;
 import Order.Modal.Response.orders.DeleteOrderResponse;
 import Order.Modal.Response.orders.OrderResponse;
 import retrofit2.Call;
 import retrofit2.http.*;
+
+import java.util.List;
 
 public interface OrderAPI {
     @GET("/api/order")
@@ -18,4 +21,6 @@ public interface OrderAPI {
     Call<DeleteOrderResponse> deleteOrder(@Path("id") String id);
     @PATCH("/api/order/{id}")
     Call<CreatedOrderResponse> updateOrder(@Path("id") String id, @Body orders Order);
+    @GET("/api/order/table/{tableId}")
+    Call<ApiResponse<List<orders>>> getOrdersByTable(@Path("tableId") String tableId);
 }
